f1d19a791aa36972994ebbcc920910f7e1da0150
Darylxyx <921218060@qq.com>
2016-10-24 19:54:33 +0800

    update

--- httpClient/app.js
+++ httpClient/app.js
@@ -1,7 +1,26 @@
 var http = require('http'),
-	url = require('url');
+	url = require('url'),
+	fs = require('fs');
 
-var req = http.request(url.parse('http://www.microsoft.com'), (res) => {
-	conosle.log('状态码: ' + res.statusCode);
-	console.log('响应头: ' + JSON.stringify(res.headers));
+var options = {
+	hostname: 'www.baidu.com',
+	port: 80,
+	path: '/',
+	method: 'GET'
+};
+
+//可以通过url.parse生成options对象
+var req = http.request(options, (res) => {
+	var out = fs.createWriteStream('./response.log');
+	out.write('状态码: ' + res.statusCode + '\n')
+	out.write('响应头: ' + JSON.stringify(res.headers) + '\n');
+	res.setEncoding('utf8');
+	res.on('data', (chunk) => {
+		out.write('响应内容: ' + chunk);
+	});
+	res.on('end', () => {
+		out.end('');
+	});
 });
+
+req.end();
