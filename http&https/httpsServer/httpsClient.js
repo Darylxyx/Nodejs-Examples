16e2936b21be07211c3701d9eae99116574fa3f8
Darylxyx <921218060@qq.com>
2016-10-26 11:02:37 +0800

    update

--- http&https/httpsServer/httpsClient.js
+++ http&https/httpsServer/httpsClient.js
@@ -1,4 +1,5 @@
-var https = require('https');
+var https = require('https'),
+	fs = require('fs');
 
 
 // 使用request请求其他网站
@@ -7,13 +8,26 @@ var opts = {
 	port: 443,
 	path: '/',
 	method: 'GET',
+	// key: fs.readFileSync('./privatekey.pem'),
+	// cert: fs.readFileSync('./certificate.pem'),
 	agent: false
 };
 
 var req = https.request(opts, (res) => {
+	console.log(res.statusCode);
 	res.on('data', (data) => {
 		console.log('请求到的内容：' + data);
 	});
 });
 
 req.end();
+
+req.on('socket', (socket) => {
+	socket.setTimeout(1000, () => {
+		req.abort();
+	});
+});
+
+req.setTimeout(1000, () => {
+	req.abort();
+});
--- http&https/httpsServer/httpsServer.js
+++ http&https/httpsServer/httpsServer.js
@@ -1,4 +1,4 @@
-	//***HTTPS服务器***
+ 	//***HTTPS服务器***
 //HTTP服务器与客户端之间传输的是明文数据，HTTPS服务器与客户端之间传输的是经过SSL安全加密后的密文数据。
 //HTTP服务器通常使用80端口或8080端口，而HTTPS服务器使用的是443端口。
 
@@ -8,6 +8,7 @@
 //2. openssl genrsa -out privatekey.pem 1024
 //3. openssl req -new -key privatekey.pem -out certrequest.csr (一路回车)
 //4. openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
+//5. openssl pkcs12 -export -in certificate.pem -inkey privatekey.pem -out certificate.pfx 
 
 var https = require('https'),
 	fs = require('fs'),
