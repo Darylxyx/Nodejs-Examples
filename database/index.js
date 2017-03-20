2f9f3eeaa4bba9b5a40bf05e3d4ffcb5ee580116
Darylxyx <921218060@qq.com>
2017-03-20 11:08:45 +0800

    update

--- database/index.js
+++ database/index.js
@@ -1,16 +1,43 @@
-var mongo = require('mongodb'),
-	server = new mongo.Server('127.0.0.1', '8888'),
-	db = new mongo.Db('databaseName', server);
+// var mongo = require('mongodb'),
+// 	server = new mongo.Server('127.0.0.1', '8888'),
+// 	db = new mongo.Db('databaseName', server);
 
-// console.log(db);
+// // console.log(db);
+
+// db.open((err, db) => {
+// 	// console.log(err, db);
+// 	if (!err) {
+// 		console.log(db);
+// 	} else {
+// 		console.log(err);
+// 	}
+// });
+
+// db.close(false, (err) => { //db.close([forceClose], [callback]) forceClose: true 强制关闭，不用能open再打开；false 非强制关闭，可用open再打开。ß
+// 	if (err) {
+// 		console.log(err);
+// 	}
+// });
+
+var mongo = require('mongodb'),
+	host = 'localhost',
+	port = mongo.Connection.DEFAULT_PORT,
+	server = new mongo.Server(host, port, {auto_reconnect: true}),
+	db = new mongo.Db('node-mongo-test', server, {safe: true});
 
 db.open((err, db) => {
-	// console.log(err, db);
-	if (!err) {
-		console.log(db);
+	if (err) {
+		throw err;
 	} else {
-		console.log(err);
+		console.log('数据库创建成功');
+		db.close();
 	}
 });
 
-db.close();
+db.on('close', (err, db) => {
+	if (err) {
+		throw err;
+	} else {
+		console.log('成功关闭数据库');
+	}
+});
