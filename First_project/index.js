16a23eb19978cbf888b80e01e665b54e77848574
Darylxyx <921218060@qq.com>
2017-04-18 16:09:59 +0800

    update

--- First_project/index.js
+++ First_project/index.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
 	res.send('Hello Express');
 });
 
+app.get('/user/:name', (req, res) => {
+	res.send('params: ' + JSON.stringify(req.params) + ', query: ' + JSON.stringify(req.query));
+});
+
 app.listen(3000);
