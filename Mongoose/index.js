75bec8f51afb8d26500de910b34f410f0f18ed39
Darylxyx <921218060@qq.com>
2017-04-25 14:37:51 +0800

    update

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -10,3 +10,42 @@ db.on('error', (err) => {
 db.once('open', (callback) => {
 	console.log('ok');
 });
+
+var kittySchema = mongoose.Schema({
+	name: String,
+	age: Number
+});
+
+kittySchema.methods.speak = function() {
+	var name = 'My name is ' + this.name;
+	console.log(name);
+};
+
+var kittyModel = mongoose.model('Kitten', kittySchema);
+
+var xyx = new kittyModel({
+	name: 'xieyuxiao',
+	age: 26
+});
+
+var datou = new kittyModel({
+	name: 'datou',
+	age: 25
+});
+
+xyx.save((err, self) => {
+	if (err) return console.error(err);
+	self.speak();
+});
+
+datou.save((err, self) => {
+	if (err) return console.error(err);
+	self.speak();
+});
+
+kittyModel.find((err, self) => {
+	if (err) return console.error(err);
+	console.log(self);
+});
+
+console.log(xyx);
