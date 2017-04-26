a5927dd833d4d5792802139fc73d71d7809c8b8a
Darylxyx <921218060@qq.com>
2017-04-26 10:43:16 +0800

    update

--- Mongoose/verify.js
+++ Mongoose/verify.js
@@ -1,19 +1,24 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/FirstBlood');
 
+var weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'firday'];
+
 var Schema = mongoose.Schema({
 	name: {type: String, unique: true, required: true},
-	age: {type: Number, min: 13, max: 19}
+	age: {type: Number, min: 13, max: 19},
+	day: {type: String, enum: weekdays}
 });
 
 var Model = mongoose.model('Kitten', Schema);
 
 Model.create({
-	name: 'verifyTest',
-	age: 20
+	name: 'verifyTest2',
+	age: 17,
+	day: 'monday'
 }, (err, user) => {
 	if (err) return console.log(err.message);
 	console.log(user);
 });
+
 
 // console.log(Schema);
