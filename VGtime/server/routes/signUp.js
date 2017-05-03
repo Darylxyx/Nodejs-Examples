4a46ab15b193699002d701446fbbe175c4181e78
Darylxyx <921218060@qq.com>
2017-05-03 14:44:56 +0800

    update

--- VGtime/server/routes/signUp.js
+++ VGtime/server/routes/signUp.js
@@ -1,15 +1,18 @@
 var express = require('express'),
 	router = express.Router(),
-	sha1 = require('sha1');
+	sha1 = require('sha1'),
+	path = require('path'),
+	fs = require('fs');
 
 var UserModel = require('../models/user'),
 	CORS = require('../middlewares/cors');
 
 router.post('/', CORS, (req, res) => {
-	let { name, password, repassword, gender, bio } = req.query,
-		avatar = req.files.avatar.path.split(path.sep).pop();
+	let { name, password, repassword, gender, bio } = req.fields,
+		avatarPath = req.files.avatar.path,
+		avatarName = req.files.avatar.name,
+		avatar = avatarPath.split(path.sep).pop();
 	
-	console.log(avatar);
 	// 检验参数
 	try {
 		if (name.length < 1 || name.length > 10) {
@@ -21,7 +24,7 @@ router.post('/', CORS, (req, res) => {
 		if (bio.length > 30) {
 			throw new Error('简介请限制在30个字符');
 		}
-		if (!req.files.avatar.name) {
+		if (!avatarName) {
 			throw new Error('缺少头像');
 		}
 		if (password !== repassword) {
@@ -31,6 +34,8 @@ router.post('/', CORS, (req, res) => {
 			throw new Error('密码至少6个字符');
 		}
 	} catch(e) {
+		// 注册失败，异步删除上传的头像
+		fs.unlink(avatarPath);
 		return global.sendResponse(res, 400, {errMsg: e.message});
 	}
 
@@ -40,14 +45,20 @@ router.post('/', CORS, (req, res) => {
 		name: name,
 		password: password,
 		gender: gender,
-		bio: bio
+		bio: bio,
+		avatar: avatar
 	};
 
 	UserModel.createUser(user)
 	.then((result) => {
-		global.sendResponse(res, 200, 'ok');
+		user = result;
+		delete user.password;
+		req.session.user = user;
+
+		global.sendResponse(res, 200, {msg: '注册成功'});
 	}).catch((err) => {
-		global.sendResponse(res, 400, err);
+		fs.unlink(avatarPath);
+		global.sendResponse(res, 400, {errMsg: err});
 	});
 
 });
