ad6c20f863ce2f784cd17c45a2be6f15ecf198e1
Darylxyx <921218060@qq.com>
2017-05-08 19:40:15 +0800

    update

--- WebSocket/server.js
+++ WebSocket/server.js
@@ -1,12 +1,16 @@
 var WebSocketServer = require('ws').Server;
 
 wss = new WebSocketServer({port: 8181});
-console.log(wss);
+// console.log(wss);
 wss.on('connection', (ws) => {
 	console.log('Connecting...');
 
-	wss.on('message', (msg) => {
+	ws.on('message', (msg) => {
 		console.log(msg);
 	});
+
+	setInterval(function() {
+		ws.send('Now time is' + new Date);
+	}, 3000);
 
 });
