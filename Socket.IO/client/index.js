777b915363d04a10b0754c31a4b102e18e270b05
Darylxyx <921218060@qq.com>
2017-05-23 11:16:47 +0800

    update

--- Socket.IO/client/index.js
+++ Socket.IO/client/index.js
@@ -4,9 +4,33 @@ var doc = document,
 	$sendBtn = doc.querySelector('.send-btn'),
 	socket = io();
 
-$sendBtn.onclick = function() {
-	var message = $input.value;
+//通知用户进入聊天室
+socket.on('user conncet', (data) => {
+	console.log(data);
+});
 
+//通知用户离开聊天室
+socket.on('user disconnect', (data) => {
+	console.log(data);
+});
+
+//接收消息
+socket.on('server message', (data) => {
+	console.log('recevie meesage: ' + JSON.stringify(data));
+	$content.innerHTML += '<div class="list"><div class="section">'+data.text+'</div></div>';
+	$content.scrollTop = $content.scrollHeight;
+});
+
+//断连通知
+socket.on('disconnect', (data) => {
+	console.log('server disconnect: ' + JSON.stringify(data));
+});
+
+$sendBtn.onclick = sendMsg;
+
+//发送消息
+function sendMsg() {
+	var message = strEscape($input.value);
 	if (!message) return;
 
 	socket.emit('client message', {
@@ -17,22 +41,16 @@ $sendBtn.onclick = function() {
 	});
 
 	$content.innerHTML += '<div class="list"><div class="section section-self">'+message+'</div></div>';
+	$content.scrollTop = $content.scrollHeight;
 	$input.value = '';
 }
 
-socket.on('user conncet', (data) => {
-	console.log(data);
-});
-
-socket.on('user disconnect', (data) => {
-	console.log(data);
-});
-
-socket.on('server message', (data) => {
-	console.log('recevie meesage: ' + JSON.stringify(data));
-	$content.innerHTML += '<div class="list"><div class="section">'+data.text+'</div></div>';
-});
-
-socket.on('disconnect', (data) => {
-	console.log('server disconnect: ' + JSON.stringify(data));
-});
+function strEscape(str){
+       var div=document.createElement('div');
+       if(div.innerText){
+           div.innerText=str;
+       }else{
+           div.textContent=str;//Support firefox
+       }
+       return div.innerHTML;
+  }
