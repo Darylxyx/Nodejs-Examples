72e73c0e2d17cb74e703b1b8f80de233f3eceae6
Darylxyx <921218060@qq.com>
2018-05-02 16:37:54 +0800

    update

--- order/server/index.js
+++ order/server/index.js
@@ -1,11 +1,38 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const logger = require('./middleware/logger');
 const app = new Koa();
 
 app.use(logger());
 
-app.use(async (ctx) => {
-    ctx.body = 'hello koa2';
+//创建根路由
+const home = new Router();
+
+home.get('/', async (ctx) => {
+    const html = `<ul>
+        <li><a href='/page/helloworld'>HelloWorld</a></li>
+        <li><a href='/page/404'>404</a></li>
+    </ul>`;
+    ctx.body = html;
 });
 
+//创建子路由
+const page = new Router();
+
+page.get('/404', async (ctx) => {
+    ctx.body = '404';
+}).get('/helloworld', async (ctx) => {
+    ctx.body = 'HelloWorld';
+});
+
+
+//装载路由
+const router = new Router();
+router.use('/', home.routes(), home.allowedMethods());
+router.use('/page', page.routes(), home.allowedMethods());
+
+app.use(router.routes()).use(router.allowedMethods());
+
 app.listen(3000);
+
+console.log('koa listening 3000 port.');
